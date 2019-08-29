134a1d7a11300eb06fe2414ff61267a8236c5129
Sven Schlarb <shsdev@users.noreply.github.com>
2019-08-29 15:46:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="github.com/eark-project/eatb",
     packages=setuptools.find_packages(),
+    package_data={'': ['logging_config.ini']},
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
