b07125737dbbbf27af3c4a96b9766332ad0a27f3
schlarbs <sven.schlarb@ait.ac.at>
2020-04-22 13:49:41 +0200

    Add the option to create a compressed container package

--- eatb/packaging/package_creator.py
+++ eatb/packaging/package_creator.py
@@ -10,16 +10,23 @@ from eatb.storage.checksum import ChecksumFile, ChecksumAlgorithm
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))  # noqa: E402
 
 
-def create_package(input_directory, packagename) -> str:
+def create_package(input_directory, packagename, output_directory=None, gunzip=False) -> str:
     """
     Create package
     :param input_directory: Input directory
+    :param packagename: Name of the package file
+    :param output_directory: Output directory (same as input directory if not provided)
+    :gunzip create compressed container package
     :return: md5checksum of package
     """
 
     # append generation number to tar file; if tar file exists, the generation number is incremented
     input_path = Path(input_directory)
-    storage_tar_file = os.path.join(input_path.parent, packagename + '.tar')
+    extension = '.tar.gz' if gunzip else '.tar'
+    if not output_directory:
+        output_directory = input_path.parent
+    storage_tar_file = os.path.join(output_directory, packagename + extension)
+    write_mode = "w:gz" if gunzip else "w:"
     tar = tarfile.open(storage_tar_file, "w:")
     total = sum([len(files) for (root, dirs, files) in os.walk(input_directory)])
     i = 0
