a1ea01c0ce714157a2d278b392ea0ce381363234
schlarbs <sven.schlarb@ait.ac.at>
2020-05-06 18:03:43 +0200

    Adapt setup configuration

--- setup.py
+++ setup.py
@@ -14,8 +14,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="github.com/eark-project/eatb",
     packages=setuptools.find_packages(),
-    include_package_data=True,
-    package_data={'': ['logging_config.ini']},
+    package_data={'': []},
+    data_files=[
+        ("settings",  ["settings/settings.cfg"]),
+        ("",  ["logging_config.ini"])
+    ],
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
