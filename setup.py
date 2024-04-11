68596599162c627318990ceca4690fd172377509
schlarbs <sven.schlarb@ait.ac.at>
2024-04-11 19:14:39 +0200

    version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eatb",
-    version="0.1.9",
+    version="0.2.0",
     author="E-ARK Foundation",
     author_email="admin@e-ark-foundation.eu",
     license='MIT',
